92d09260ee5b8a2d2faf78c5de2a145c8bc6a65f
Marek Kadek <kadek.marek@gmail.com>
2016-10-24 10:56:05 +1300

    proxy settings applied per request

--- modules/scraper/src/main/scala/com/kadekm/scraper/browser.scala
+++ modules/scraper/src/main/scala/com/kadekm/scraper/browser.scala
@@ -1,28 +1,10 @@
 package com.kadekm.scraper
 
-import fs2._
+import com.kadekm.scraper.proxy.ProxySettings
 import fs2.util._
 
-import fs2.util.syntax._
-
 abstract class Browser[F[_]](implicit FI: Effect[F]) {
-  val currentProxy: ProxySettings
+  val proxySettings: Option[ProxySettings]
 
   def fromUrl(url: String): F[Document]
-
-  protected def withProxy[A](f: => F[A]): F[A] =
-    for {
-      before <- proxy.getProxySettings[F]
-      _      <- proxy.setProxySettings(currentProxy)
-
-      // todo: not-good - mutating global state (even though in task)
-      // multiple browsers running concurrently in single jvm and you're in
-      // for a bad time
-
-      // Reset to `before` even on error
-      r <- FI.attempt(f).flatMap {
-        case Right(x) => proxy.setProxySettings(before).map(_ => x)
-        case Left(e)  => proxy.setProxySettings(before).flatMap(_ => FI.fail[A](e))
-      }
-    } yield r
 }
--- modules/scraper/src/main/scala/com/kadekm/scraper/proxy.scala
+++ modules/scraper/src/main/scala/com/kadekm/scraper/proxy.scala
@@ -1,131 +1,23 @@
 package com.kadekm.scraper
 
-import fs2.util.Suspendable
+import java.net._
 
-import fs2.util.syntax.FunctorOps
-import fs2.util.syntax.MonadOps
+object proxy {
+  sealed abstract case class ProxySettings(proxy: Proxy)
 
-final case class ProxyTuple(host: String, port: Int)
+  object ProxySettings {
 
-final case class ProxySettings(httpProxy: Option[ProxyTuple],
-                               httpsProxy: Option[ProxyTuple],
-                               socksProxy: Option[ProxyTuple]) {}
+    def http(host: String, port: Int): ProxySettings = {
+      val proxy = new Proxy(Proxy.Type.HTTP, InetSocketAddress.createUnresolved(host, port))
+      new ProxySettings(proxy){}
+    }
 
-private[scraper] object proxy {
+    def socks(address: InetSocketAddress): ProxySettings = {
+      val proxy = new Proxy(Proxy.Type.SOCKS, address)
+      new ProxySettings(proxy){}
+    }
 
-  private[this] val HTTP_PROXY_HOST: String  = "http.proxyHost"
-  private[this] val HTTP_PROXY_PORT: String  = "http.proxyPort"
-  private[this] val HTTPS_PROXY_PORT: String = "https.proxyPort"
-  private[this] val HTTPS_PROXY_HOST: String = "https.proxyHost"
-  private[this] val SOCKS_PROXY_HOST: String = "socksProxyHost"
-  private[this] val SOCKS_PROXY_PORT: String = "socksProxyPort"
+    def none: ProxySettings = new ProxySettings(java.net.Proxy.NO_PROXY) {}
 
-  private[this] type FF[F[_]] = Suspendable[F]
-
-  /**
-    * Sets the JVM-wide HTTP proxy configuration.
-    * @param host the proxy host
-    * @param port the proxy port
-    */
-  def setHttpProxy[F[_]](host: String, port: Int)(implicit FI: FF[F]): F[Unit] = FI.delay {
-    System.setProperty(HTTP_PROXY_HOST, host)
-    System.setProperty(HTTP_PROXY_PORT, String.valueOf(port))
   }
-
-  /**
-    * Sets the JVM-wide HTTP proxy configuration.
-    * @param host the proxy host
-    * @param port the proxy port
-    */
-  def setHttpsProxy[F[_]](host: String, port: Int)(implicit FI: FF[F]): F[Unit] = FI.delay {
-    System.setProperty(HTTPS_PROXY_HOST, host)
-    System.setProperty(HTTPS_PROXY_PORT, String.valueOf(port))
-  }
-
-  /**
-    * Returns the current JVM-wide HTTP proxy configuration.
-    * @return the current JVM-wide HTTP proxy configuration.
-    */
-  def getHttpProxy[F[_]](implicit FI: FF[F]): F[Option[ProxyTuple]] = FI.delay {
-    for {
-      host <- Option(System.getProperty(HTTP_PROXY_HOST))
-      port <- Option(System.getProperty(HTTP_PROXY_PORT))
-    } yield ProxyTuple(host, port.toInt)
-  }
-
-  /**
-    * Returns the current JVM-wide HTTPS proxy configuration.
-    * @return the current JVM-wide HTTPS proxy configuration.
-    */
-  def getHttpsProxy[F[_]](implicit FI: FF[F]): F[Option[ProxyTuple]] = FI.delay {
-    for {
-      host <- Option(System.getProperty(HTTPS_PROXY_HOST))
-      port <- Option(System.getProperty(HTTPS_PROXY_PORT))
-    } yield ProxyTuple(host, port.toInt)
-  }
-
-  /**
-    * Unsets the JVM-wide HTTP proxy configuration.
-    */
-  def removeHttpProxy[F[_]](implicit FI: FF[F]): F[Unit] = FI.delay {
-    System.clearProperty(HTTP_PROXY_HOST)
-    System.clearProperty(HTTP_PROXY_PORT)
-  }
-
-  /**
-    * Unsets the JVM-wide HTTPS proxy configuration.
-    */
-  def removeHttpsProxy[F[_]](implicit FI: FF[F]): F[Unit] = FI.delay {
-    System.clearProperty(HTTPS_PROXY_HOST)
-    System.clearProperty(HTTPS_PROXY_PORT)
-  }
-
-  /**
-    * Sets the JVM-wide SOCKS proxy configuration.
-    * @param host the proxy host
-    * @param port the proxy port
-    */
-  def setSocksProxy[F[_]](host: String, port: Int)(implicit FI: FF[F]): F[Unit] = FI.delay {
-    System.setProperty(SOCKS_PROXY_HOST, host)
-    System.setProperty(SOCKS_PROXY_PORT, String.valueOf(port))
-  }
-
-  /**
-    * Returns the current JVM-wide SOCKS proxy configuration.
-    * @return the current JVM-wide SOCKS proxy configuration.
-    */
-  def getSocksProxy[F[_]](implicit FI: FF[F]): F[Option[ProxyTuple]] = FI.delay {
-    for {
-      host <- Option(System.getProperty(SOCKS_PROXY_HOST))
-      port <- Option(System.getProperty(SOCKS_PROXY_PORT))
-    } yield ProxyTuple(host, port.toInt)
-  }
-
-  /**
-    * Unsets the JVM-wide SOCKS proxy configuration.
-    */
-  def removeSocksProxy[F[_]](implicit FI: FF[F]): F[Unit] = FI.delay {
-    System.clearProperty(SOCKS_PROXY_HOST)
-    System.clearProperty(SOCKS_PROXY_PORT)
-  }
-
-  /**
-    * Sets proxy to system
-    */
-  def setProxySettings[F[_]](settings: ProxySettings)(implicit FI: FF[F]): F[Unit] =
-    for {
-      _ <- settings.httpProxy.fold(FI.pure[Unit](()))(x => setHttpProxy(x.host, x.port))
-      _ <- settings.httpsProxy.fold(FI.pure[Unit](()))(x => setHttpsProxy(x.host, x.port))
-      _ <- settings.socksProxy.fold(FI.pure[Unit](()))(x => setHttpsProxy(x.host, x.port))
-    } yield ()
-
-  /**
-    * Reads current proxy settings
-    */
-  def getProxySettings[F[_]](implicit FI: FF[F]): F[ProxySettings] =
-    for {
-      http  <- getHttpProxy
-      https <- getHttpsProxy
-      socks <- getSocksProxy
-    } yield ProxySettings(http, https, socks)
 }
