f357b9b8a5900f4b20124c5771188b5e22d3d59b
Marek Kadek <kadek.marek@gmail.com>
2016-10-23 21:06:22 +1300

    parallel crawler

--- modules/scrawler/src/test/scala/com/kadekm/scrawler/SomeTest.scala
+++ modules/scrawler/src/test/scala/com/kadekm/scrawler/SomeTest.scala
@@ -2,8 +2,7 @@ package com.kadekm.scrawler
 
 import com.kadekm.scraper._
 import com.kadekm.scraper.jsoup.JsoupBrowser
-import com.kadekm.scrawler.dsl._
-import com.kadekm.scraper.extractors._
+import com.kadekm.scrawler.crawlers._
 
 import fs2._
 
@@ -11,15 +10,13 @@ class SomeTest extends ScrawlerTest {
 
   // todo: proper tests
 
+  implicit val strategy: Strategy = Strategy.fromFixedDaemonPool(128, "foo")
+  val browser = JsoupBrowser[Task]
+
   "something" - {
-    "foo" in {
-      implicit val strategy: Strategy = Strategy.fromFixedDaemonPool(8, "foo")
-      val browser = JsoupBrowser[Task]
-
-      val wikiCrawler = new Crawler[Task, String](browser) {
+    "sequential crawler" ignore {
+      val wikiCrawler = new SequentialCrawler[Task, String](browser) {
         override val url = "https://blog.scrapinghub.com"
-
-        //override def maxOpenConnections: Int = 8
 
         override protected def onDocument(document: Document): Stream[Pure, Yield[String]] = {
           val entries = for (title <- document.root.select("h2.entry-title"))
@@ -40,6 +37,52 @@ class SomeTest extends ScrawlerTest {
       }
 
       val result = wikiCrawler.stream.runLog.unsafeRun
+      println(result.mkString("\n\n"))
+      println("done")
+    }
+
+    "sequential crawler on wiki links" ignore {
+      val wikiCrawler = new SequentialCrawler[Task, String](browser) {
+        override val url = "https://en.wikipedia.org/wiki/Main_Page"
+
+        override protected def onDocument(document: Document): Stream[Pure, Yield[String]] = {
+          val title = for (title <- document.root.select("head > title"))
+            yield YieldData(title.text)
+
+          val externalLinks = document.root.select("a[href^='https://']")
+            .map(_.attr("href").get)
+            .map(Visit)
+            .toSeq
+
+          Stream.emits(title.toSeq) ++ Stream.emits(externalLinks)
+        }
+      }
+
+      val result = wikiCrawler.stream.take(20).runLog.unsafeRun
+      println(result.mkString("\n\n"))
+      println("done")
+    }
+
+    "parallel crawler on wiki links" ignore {
+      val wikiCrawler = new ParallelCrawler[Task, String](browser) {
+        override val url = "https://en.wikipedia.org/wiki/Main_Page"
+
+        override def maxOpenConnections: Int = 30
+
+        override protected def onDocument(document: Document): Stream[Pure, Yield[String]] = {
+          val title = for (title <- document.root.select("head > title"))
+            yield YieldData(title.text)
+
+          val externalLinks = document.root.select("a[href^='https://']")
+            .map(_.attr("href").get)
+            .map(Visit)
+            .toSeq
+
+          Stream.emits(title.toSeq) ++ Stream.emits(externalLinks)
+        }
+      }
+
+      val result = wikiCrawler.stream.take(20).runLog.unsafeRun
       println(result.mkString("\n\n"))
       println("done")
     }
