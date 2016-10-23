179180f50048bbbad495891c5fae03047d4c653a
Marek Kadek <kadek.marek@gmail.com>
2016-10-23 19:44:22 +1300

    implement simpler crawling

--- modules/scrawler/src/main/scala/com/kadekm/scrawler/dsl/Crawler.scala
+++ modules/scrawler/src/main/scala/com/kadekm/scrawler/dsl/Crawler.scala
@@ -5,71 +5,34 @@ import fs2._
 import fs2.util._
 import fs2.util.syntax._
 
-// todo: tests
-sealed trait Decision
-final case class YieldData[A](a: A) extends Decision
-final case class YieldMultipleData[A](as: Seq[A]) extends Decision
-final case class VisitUrl(url: String) extends Decision
-final case class VisitUrls(url: Seq[String]) extends Decision
-final case class ParallelVisitUrls(url: Seq[String]) extends Decision
-case object Pass extends Decision
-
-// todo: revisit
-sealed trait Result[+A]
-final case class Data[A](a: A) extends Result[A]
-final case class MultipleData[A](as: Seq[A]) extends Result[A]
-case object NoResult extends Result[Nothing]
+sealed trait Yield[+A]
+final case class YieldData[A](a: A) extends Yield[A]
+final case class Visit(url: String) extends Yield[Nothing]
 
 abstract class Crawler[F[_], A](browser: Browser[F])(implicit FI: Async[F]) {
 
   def url: String
 
-  def maxOpenConnections: Int
+  //def maxOpenConnections: Int
 
-  // todo: can we do only single operation?
-  def onElement(document: Document): PartialFunction[Element, Decision]
-
-  private def safeOnElement(document: Document): Element => Decision = onElement(document).lift.andThen(_.getOrElse(Pass))
+  protected def onDocument(document: Document): Stream[Pure, Yield[A]]
 
   final def stream: Stream[F, A] =
-    Stream.unfoldEval[F, List[Decision], Result[A]](List(VisitUrl(url))) {
-
-      case VisitUrl(urll) :: rest => browser.fromUrl(urll).map { doc =>
-        // todo: we probably shouldn't run it on each element but have different mechanism
-        val nextDecisions = doc.root.children.map(safeOnElement(doc)).toList
-        Some((NoResult, rest ++ nextDecisions))
-      }
-
-      case VisitUrls(urls) :: rest => FI.pure {
-        val visits = urls.map(VisitUrl)
-        Some((NoResult, rest ++ visits))
-      }
-
-      case ParallelVisitUrls(urls) :: rest =>
-        concurrent.join(maxOpenConnections) {
-          val par = urls.map(url => Stream.eval(browser.fromUrl(url)))
-          Stream.emits(par)
-        }.runLog.map { docs =>
-          val nextDecisions = docs.flatMap(doc => doc.root.children.map(safeOnElement(doc))).toList
-          Some((NoResult, rest ++ nextDecisions))
+    Stream.unfoldEval[F, List[Yield[A]], Yield[A]](List[Yield[A]](Visit(url))) {
+      case (u@Visit(url)) :: rest =>
+        browser.fromUrl(url).map(onDocument).map(_.toList).map { xs =>
+          Some((u, rest ::: xs))
         }
 
-      // todo: castings no-no-no!
-      case YieldData(x) :: rest => FI.pure {
-        Some((Data(x.asInstanceOf[A]), rest))
+      case (u@YieldData(data)) :: rest => FI.pure {
+        Some((u, rest))
       }
 
-      case YieldMultipleData(xs) :: rest => FI.pure {
-        Some((MultipleData(xs.asInstanceOf[Seq[A]]), rest))
-      }
-
-      // todo: implement rest of ops
       case _ => FI.pure(None)
     }
-    .collect {  // represent it all as nested
-      case Data(x) => Seq(x.asInstanceOf[A])
-      case MultipleData(xs) => xs
+    .collect {
+      // we are only interested in data that we had yielded
+      case YieldData(a) => a
     }
-    .flatMap(Stream.emits) // flatten it all (do we want to?)
 
 }
