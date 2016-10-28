8717e399739a0429172bd53dee83867e7a5d0e0f
Marek Kadek <kadek.marek@gmail.com>
2016-10-29 08:47:15 +1300

    gives ability to configure browser with more options

--- modules/scraper/src/main/scala/com/marekkadek/scraper/jsoup/browser.scala
+++ modules/scraper/src/main/scala/com/marekkadek/scraper/jsoup/browser.scala
@@ -5,19 +5,21 @@ import com.marekkadek.scraper.proxy.ProxySettings
 import fs2.util._
 import org.jsoup.{Connection, Jsoup}
 
-sealed class JsoupBrowser[F[_]] private (val proxySettings: Option[ProxySettings] = Option.empty)(
-    implicit FI: Effect[F])
+sealed class JsoupBrowser[F[_]] private (val proxySettings: Option[ProxySettings] = Option.empty,
+                                         userAgent: String = "Mozilla",
+                                         followRedirects: Boolean = true,
+                                         validateTLSCertificates: Boolean = true)(implicit FI: Effect[F])
     extends Browser[F] {
   override def fromUrl(url: String): F[Document] =
     FI.delay {
-      //println(s"${Thread.currentThread.getName} > $url") // todo: remove :)
-
       val con = Jsoup.connect(url)
 
-      con.followRedirects(true)
+      con.userAgent(userAgent)
+      con.followRedirects(followRedirects)
+      con.validateTLSCertificates(validateTLSCertificates)
+      con.ignoreHttpErrors(true) // do not throw exceptions in `execute`
       proxySettings.foreach(x => con.proxy(x.proxy))
 
-      // todo (#19) execute() may throw exceptions
       val r = con.execute()
 
       JsoupDocument(r.parse)
@@ -27,6 +29,11 @@ sealed class JsoupBrowser[F[_]] private (val proxySettings: Option[ProxySettings
 object JsoupBrowser {
   def apply[F[_]: Effect]                       = new JsoupBrowser[F]
   def apply[F[_]: Effect](proxy: ProxySettings) = new JsoupBrowser[F](Some(proxy))
+  def apply[F[_]: Effect](proxySettings: Option[ProxySettings] = Option.empty,
+                          userAgent: String = "Mozilla",
+                          followRedirects: Boolean = true,
+                          validateTLSCertificates: Boolean = true) =
+    new JsoupBrowser[F](proxySettings, userAgent, followRedirects, validateTLSCertificates)
 
   def readInputStream[F[_]](is: java.io.InputStream, charset: java.nio.charset.Charset, baseUri: String)(
       implicit FI: Effect[F]): F[Document] = FI.delay {
--- modules/scrawler/src/test/scala/com/marekkadek/scrawler/opencrawling.scala
+++ modules/scrawler/src/test/scala/com/marekkadek/scrawler/opencrawling.scala
@@ -8,11 +8,11 @@ object opencrawling {
   private[this] val random = new Random
 
   val urls: Seq[String] = Seq(
-    //"https://news.ycombinator.com", //
+    "https://news.ycombinator.com",
     "http://www.github.com",
-    "http://www.wikipedia.org"
-    //"http://www.stackoverflow.com" //
-    //"http://reddit.com" //
+    "http://www.wikipedia.org",
+    "http://www.stackoverflow.com"
+    //"http://reddit.com"
   )
 
   def randomUrl: Task[String] = Task.delay {
